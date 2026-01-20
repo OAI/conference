b6c47b9be63302b5a8425e2a71f097094636e965
Juri Jatschmenow <juri.jatschmenow@raiok.com>
2026-01-20 17:27:10 +0100

    move paris to public

--- src/data/previousEvents.js
+++ src/data/previousEvents.js
@@ -8,7 +8,7 @@ export const previousEvents = [
   {
     name: "OpenAPI  <br/>Conference in <br/>Paris",
     key: "paris-2025",
-    url: "/paris2025/",
+    url: "./paris2025/",
     date: "11 December, 2025",
   },
 ];
